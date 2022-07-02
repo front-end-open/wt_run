b3cd3f03bd565195e56fc7fbf1bcb5196c74e9d8
wangshan <22212@qq.com>
2022-07-03 00:11:38 +0800

    refactor: 更改ts模块导入扩展名

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,14 +1,14 @@
 /*
- * @LastEditTime: 2022-07-02 20:18:31
+ * @LastEditTime: 2022-07-02 23:49:51
  * @Description:
  * @Date: 2022-07-02 20:17:15
  * @Author: wangshan
  * @LastEditors: wangshan
  */
-export function sum(a, b) {
+export function sum(a: number, b: number): number {
   return a + b;
 }
 
-export function log(msg) {
+export function log(msg: string): void {
   console.log(msg);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 /*
- * @LastEditTime: 2022-07-02 23:25:51
+ * @LastEditTime: 2022-07-02 23:51:55
  * @Description:
  * @Date: 2022-07-02 20:17:00
  * @Author: wangshan
  * @LastEditors: wangshan
  */
-import { sum, log } from "@/common/util.ts";
+
+import { sum, log } from "@/common/util";
 
 log(sum(4, 5));
