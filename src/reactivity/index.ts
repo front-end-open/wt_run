5aa55f8556c23a73d19bf25ce2139a4472be49e3
wangshan <22212@qq.com>
2022-07-03 03:11:23 +0800

    feat: 添加视图更新

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-03 02:50:49
+ * @LastEditTime: 2022-07-03 03:07:47
  * @Description:
  * @Date: 2022-07-03 02:40:23
  * @Author: wangshan
@@ -14,5 +14,17 @@ interface Man<T> {
 
 const _jieke: Man<number> = { a: 1, b: 2 };
 const { a, b } = _jieke;
+const arr: Array<number> = [1, 2, 3, 4, 5];
 
 log(a + b);
+
+const UL: HTMLUListElement = document.createElement("ul");
+
+arr.forEach((v) => {
+  const LI: HTMLLIElement = document.createElement("li");
+  LI.innerHTML = v + "";
+  UL.appendChild(LI);
+  document.body.appendChild(UL);
+});
+
+log(arr.at(-1));
