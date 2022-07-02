bd0a72470a6f7d9dfdb1fb9990815ea576767ea8
wangshan <22212@qq.com>
2022-07-02 21:49:10 +0800

    chore: update webpack.base.js

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,11 +1,12 @@
 /*
- * @LastEditTime: 2022-07-02 21:06:03
+ * @LastEditTime: 2022-07-02 21:46:04
  * @Description:
  * @Date: 2022-07-02 20:14:23
  * @Author: wangshan
  * @LastEditors: wangshan
  */
 const path = require("path");
+const WebpackBar = require("webpackbar");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -26,6 +27,12 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "/public/index.html",
+    }),
+    new WebpackBar({
+      // 构建进度条
+      color: "#85d", // 默认green，进度条颜色支持HEX
+      basic: false, // 默认true，启用一个简单的日志报告器
+      profile: false, // 默认false，启用探查器。
     }),
   ],
 };
