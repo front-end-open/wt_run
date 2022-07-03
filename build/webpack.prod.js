b03e504a00f2670340fc1c50246a9f4b13f46a68
wangshan <22212@qq.com>
2022-07-04 01:41:50 +0800

    feat: 重复的node_module抽离chunk

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-03 07:37:38
+ * @LastEditTime: 2022-07-04 01:26:47
  * @Description: production
  * @Date: 2022-07-02 20:14:29
  * @Author: wangshan
@@ -22,5 +22,23 @@ module.exports = smp.wrap(
         generateStatsFile: true,
       }),
     ],
+    optimization: {
+      // 代码分离, 可快可共享chunk,抽离到单独的chunk
+      splitChunks: {
+        // include all types of chunks
+        chunks: 'all',
+        // 重复打包问题
+        cacheGroups: {
+          vendors: {
+            // node_modules里的代码
+            test: /[\\/]node_modules[\\/]/,
+            chunks: 'all',
+            // name: 'vendors', 一定不要定义固定的name
+            priority: 10, // 优先级
+            enforce: true,
+          },
+        },
+      },
+    },
   })
 );
