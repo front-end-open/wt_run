9637141a66844cef9c06925e206376121886fb7f
wangshan <22212@qq.com>
2022-07-10 02:17:04 +0800

    deps: add Jest

--- src/computed/index.ts
+++ src/computed/index.ts
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-10 00:51:24
+ * @LastEditTime: 2022-07-10 02:15:39
  * @Description: computed and lazy
  * @Date: 2022-07-10 00:45:14
  * @Author: wangshan
@@ -7,6 +7,4 @@
  */
 
 // 为副作用调度函数effectV2传递可选参数lazy，标志副作用函数调度可以按需调度
-// 目标
-
-// 重新设计effectV2,引入lazy参数的逻辑
+import { effectV2, obj } from '@/utils/common';
