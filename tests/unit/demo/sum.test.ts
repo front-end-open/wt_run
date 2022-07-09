29ff2698c77af9c83b45389150a6b668e18ee409
wangshan <22212@qq.com>
2022-07-10 04:14:05 +0800

    feat: add 类型模块
    - types/jest
    - types/node

--- tests/unit/demo/sum.test.ts
+++ tests/unit/demo/sum.test.ts
@@ -1,12 +1,12 @@
 /*
- * @LastEditTime: 2022-07-10 03:54:24
+ * @LastEditTime: 2022-07-10 04:02:18
  * @Description: 求和测试
  * @Date: 2022-07-10 03:49:07
  * @Author: wangshan
  * @LastEditors: wangshan
  */
 
-const sum = require('@/');
+const sum = require('@/sum/sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
