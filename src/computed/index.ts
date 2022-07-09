2a00855ae3222b790daf88f139da3e3edc53ccdb
wangshan <22212@qq.com>
2022-07-10 00:54:26 +0800

    docs: 添加computed模块的说明文件

---
+++ src/computed/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @LastEditTime: 2022-07-10 00:51:24
+ * @Description: computed and lazy
+ * @Date: 2022-07-10 00:45:14
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+
+// 为副作用调度函数effectV2传递可选参数lazy，标志副作用函数调度可以按需调度
+// 目标
+
+// 重新设计effectV2,引入lazy参数的逻辑
