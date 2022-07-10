33e2f439fb0a62fa563acc1f6f067c50dc822a16
wangshan <22212@qq.com>
2022-07-10 23:53:15 +0800

    docs: 计算属性类型说明

--- src/computed/utilss/computed.d.ts
+++ src/computed/utilss/computed.d.ts
@@ -1,10 +1,14 @@
 /*
- * @LastEditTime: 2022-07-10 23:47:44
+ * @LastEditTime: 2022-07-10 23:52:11
  * @Description: 计算属性api类型
  * @Date: 2022-07-10 22:28:59
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+
+// 本地模型文件
+// 模块类型文件
+// 全局声明文件，不能使用export, import 关键字。否则会被作为模块处理
 export interface ComputedS {
   (getter: effecFn): void;
 }
