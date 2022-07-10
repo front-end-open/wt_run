3e531f07ba86861880e01ccf6dd8d43d0a302429
wangshan <22212@qq.com>
2022-07-10 22:22:13 +0800

    deps: library vue

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,17 +1,18 @@
 /*
- * @LastEditTime: 2022-07-04 00:41:24
+ * @LastEditTime: 2022-07-10 22:18:28
  * @Description:
  * @Date: 2022-07-02 20:17:15
  * @Author: wangshan
  * @LastEditors: wangshan
  */
 import moment from 'moment';
+import { ComputedRef } from 'vue';
 
 export function sum(a: number, b: number): number {
   return a + b;
 }
 
-export function log(msg: string | number): void {
+export function log(msg: string | number | ComputedRef): void {
   console.log(msg);
   const title: HTMLElement = document.createElement('h1');
   title.innerHTML = moment().format('YYYY-MM-DD');
--- src/computed/index.ts
+++ src/computed/index.ts
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-10 02:40:14
+ * @LastEditTime: 2022-07-10 22:19:26
  * @Description: computed and lazy
  * @Date: 2022-07-10 00:45:14
  * @Author: wangshan
@@ -7,12 +7,16 @@
  */
 
 // 为副作用调度函数effectV2传递可选参数lazy，标志副作用函数调度可以按需调度
+import { computed, ref } from 'vue';
+
 import { log } from '@/common/util';
 import { effectV2, obj } from '@/utils/common';
 
 effectV2(() => {
   console.log('非lazy，立即调度', obj.text);
 });
+
+const a = ref(1);
 
 const effect = effectV2(
   () => {
@@ -26,3 +30,5 @@ const effect = effectV2(
 );
 
 log(effect() as number);
+
+log(computed(() => a));
