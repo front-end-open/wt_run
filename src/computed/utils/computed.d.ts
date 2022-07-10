ccd6a028a1563a7e753958de47b9ed77add274fa
wangshan <22212@qq.com>
2022-07-10 22:33:56 +0800

    chore: test coverage

---
+++ src/computed/utils/computed.d.ts
@@ -0,0 +1,7 @@
+/*
+ * @LastEditTime: 2022-07-10 22:28:59
+ * @Description: 计算属性api类型
+ * @Date: 2022-07-10 22:28:59
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
---
+++ src/computed/utils/computed.ts
@@ -0,0 +1,7 @@
+/*
+ * @LastEditTime: 2022-07-10 22:31:24
+ * @Description:
+ * @Date: 2022-07-10 22:28:50
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
