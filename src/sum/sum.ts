f322735dd9814619713c37274cb86d4c60d05c69
wangshan <22212@qq.com>
2022-07-10 18:29:11 +0800

    test: 测试报告

--- src/sum/sum.ts
+++ src/sum/sum.ts
@@ -1,11 +1,10 @@
 /*
- * @LastEditTime: 2022-07-10 03:47:51
+ * @LastEditTime: 2022-07-10 17:42:17
  * @Description:
  * @Date: 2022-07-10 03:47:49
  * @Author: wangshan
  * @LastEditors: wangshan
  */
-function sum(a: number, b: number): number {
+export function sum(a: number, b: number): number {
   return a + b;
 }
-module.exports = sum;
--- tests/unit/demo/sum.test.ts
+++ tests/unit/demo/sum.test.ts
@@ -1,13 +1,13 @@
 /*
- * @LastEditTime: 2022-07-10 16:58:23
+ * @LastEditTime: 2022-07-10 18:20:05
  * @Description: 求和测试
  * @Date: 2022-07-10 03:49:07
  * @Author: wangshan
  * @LastEditors: wangshan
  */
 
-// const sum = require('../sum/sum');
+import { sum } from '../../../src/sum/sum';
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
