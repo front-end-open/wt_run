84546e3f06708bf0bda554a59a494534e1895b89
wangshan <22212@qq.com>
2022-07-12 20:47:38 +0800

    feat: 解决外层副作用更新问题

--- src/computed/index.ts
+++ src/computed/index.ts
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-12 01:01:07
+ * @LastEditTime: 2022-07-12 20:47:14
  * @Description: computed and lazy
  * @Date: 2022-07-10 00:45:14
  * @Author: wangshan
@@ -48,17 +48,20 @@ import { ccomputed } from './utilss/computedD';
 // log((res as { value: string | number }).value); // 再次访问 value, 没有更新 3, 预期应该是4
 
 /**
- *  更新obj.st 后再次访问计算属性没有更新，原因在于第一次访问计算属性value后，dirty的状态已经被重置为false，表示不需要更新。
+ *  更新obj.st 后再次访问计算属性没有更新，原因在于第一次访问计算属性value后，dirty的状态已经被重置为false，由于计算属性返回引用导致上下文环境一致没有被销毁,表示不需要更新。
  *
  *  解决: 为副作用设置调度器，每次更新后，重置dirty状态.
  */
 
-// 副作用嵌套，计算属性不更新
+// 副作用嵌套，外层副作用函数不调度.
+// 此时计算属性已更新
+
 const res = ccomputed(() => {
   return (obj.st as number) + (obj.step as number);
 });
 
 effectV2(() => {
+  log('外层副作用调度');
   log(
     (
       res as {
@@ -69,3 +72,15 @@ effectV2(() => {
 });
 
 (obj.st as number)++;
+log(
+  (
+    res as {
+      value: number;
+    }
+  ).value
+);
+
+/**
+ * Vue的template最终被编译成渲染函数，以供在运行时调用, 做真实dom绘制和渲染。渲染函数内部的虚拟dom，涉及数据绑定，属于带有副作用的更新。此时如果带有计算属性绑定的vnode在，当计算属性更新时，将会导致页面更新.
+ *  上述是建立在正常情况来说的，在目前自发简历的响应式系统来看，如果存在副作用嵌套【组件的嵌套就属于副作用嵌套的体现】。如果此时虚拟dom更新，会存在外层副作用附件不更新的情况. 只会触发内层计算属性内部副作用函数的更新.
+ */
--- src/computed/utilss/computedD.ts
+++ src/computed/utilss/computedD.ts
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-12 01:05:08
+ * @LastEditTime: 2022-07-12 20:31:11
  * @Description:
  * @Date: 2022-07-10 22:28:50
  * @Author: wangshan
@@ -7,7 +7,7 @@
  */
 
 import { ComputedS } from './computed';
-import { effectV2, trigger } from '../../utils/common';
+import { effectV2, trigger, track } from '../../utils/common';
 
 export const ccomputed: ComputedS = (getter: effecFn) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -18,22 +18,26 @@ export const ccomputed: ComputedS = (getter: effecFn) => {
     get value() {
       if (dirty) {
         value = effecFn(); // 缓存结果
+        console.log('更新');
 
         // 重置标志量
         dirty = false;
       }
+      console.log('not更新');
+      track(obj, 'value'); // 手动调用track函数进行追踪
 
       return value;
     },
   };
-  // getter看做是外部传入的真是effect
+  // getter看做是外部传入的真是 effect
   const effecFn = effectV2(getter, {
     lazy: true,
     schduler() {
-      dirty = true; // 每次调用完，重置dirty状态。避免后续更新。dirty一直为false。
+      if (!dirty) {
+        dirty = true; // 每次调用完，重置dirty状态。避免后续更新。dirty一直为false。
 
-      // 当计算属性所依赖的数据更新时，调用trigger手动更新value
-      trigger(obj, 'value');
+        trigger(obj, 'value'); // 当计算属性所依赖的数据更新时，调用trigger手动更新value; 同时这里也避免了，如果不存在更新时，也不需要进行追踪调度.
+      }
     },
   });
 
